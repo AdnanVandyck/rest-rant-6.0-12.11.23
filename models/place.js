391129c3f73a04175a2fdb51a68dbd5f5385f95e
Adnan <adnanvandyck@gmail.com>
2023-12-12 12:37:28 -0500

    sending index and show data

---
+++ models/place.js
@@ -0,0 +1,16 @@
+module.exports = [
+    {
+      name: 'PANDA',
+      city: 'Harlem',
+      state: 'NY',
+      cuisines: 'Asian Fusion',
+      image: 'https://unsplash.com/photos/body-of-water-during-sunset-Xbf_4e7YDII'
+    }, 
+    {
+      name: 'Blu On The Hudson',
+      city: 'Weehawken',
+      state: 'NJ',
+      cuisines: 'Steakhouse, Suishi',
+      image: 'https://unsplash.com/photos/body-of-water-during-sunset-Xbf_4e7YDII'
+    }]
+    
